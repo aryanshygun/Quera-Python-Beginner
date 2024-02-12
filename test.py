1c101d6473bc6e6fcafa370b8abbbd8c2d3f4e89
aryanshygun <aryanshygun5272@gmail.com>
2024-02-12 05:28:57 +0330

    incomplete

--- test.py
+++ test.py
@@ -1,19 +1,4 @@
+num = 1
 
-
-ans = tuple(map(int, input().split()))
-
-a = []
-for i in ans:
-    a.append(i)
-    
-for i in range(0,len(a)-1):
-    if a[0] < a[1]:
-        a.remove(a[0])
-        # print(a)
-    else:
-        a.remove(a[1])
-        # print(a)
-        
-for i in range(1, len(ans)+1):
-    if a[0] == ans[i-1]:
-        print(i)
+for i in range(1, num + 1):
+    print(i)
