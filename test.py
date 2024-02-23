1f41e396ae1d4b66069f3a4d642f2c522d69aebf
aryanshygun <aryanshygun5272@gmail.com>
2024-02-23 22:51:13 +0330

    commit

--- test.py
+++ test.py
@@ -1,16 +1,12 @@
-En = {1: 'One', 2: 'Two' , 3: 'Three', 4: 'Four'  , 5: 'Five' }
-Fr  = {1: 'Un' , 2: 'Deux', 3: 'Trois', 4: 'Quatre', 5: 'Cinq'}
-x = input()
+def say_hello_bye(n):
+    if n == 1:
+        print(n, "Hello")
+        print(n, "Bye!")
+        return
 
-xlist = list()
-while x != 'End':
-    a, b = x.split()
-    a = int(a)
-    if b == 'En':
-        xlist.append(En.get(a))
-    if b == 'Fr':
-        xlist.append(Fr.get(a))
-    x = input()
+    print(n, "Hello")
+    say_hello_bye(n - 1)
+    print(n, "Bye!")
 
-for i in xlist:
-    print(i)
+n = int(input())
+say_hello_bye(n)
--- test2.py
+++
@@ -1,1 +0,0 @@
-# this is me testing git
